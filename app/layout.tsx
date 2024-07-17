9775955f5ae56dd37e6b4184b6904675c5fdb46c
Md. Najib Islam <najibivy1@gmail.com>
2024-07-17 12:45:25 +0600

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Tech Shop",
+  description: "Get tech products at your fingertips",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
